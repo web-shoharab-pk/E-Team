e28a75d016a85af70ff73d33361c4cb71e8b528f
iraj munshy <munshyiraj@gmail.com>
2021-07-23 18:30:16 +0600

    home area update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App text-center">
-     <h1 className="text-9xl">Passionate Coders</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
