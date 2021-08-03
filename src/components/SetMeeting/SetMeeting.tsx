701eecd39c43ff084c0be043c4a8b45899414f3a
MasumBilla19 <masumiraj0218@gmail.com>
2021-08-03 10:21:54 +0600

    set meeting

--- src/components/SetMeeting/SetMeeting.tsx
+++ src/components/SetMeeting/SetMeeting.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import Topbar from "../Topbar/Topbar";
-import {Link} from 'react-router-dom';
 
 const SetMeeting = () => {
   return (
@@ -13,83 +12,6 @@ const SetMeeting = () => {
         <Topbar />
       </div>
 
-      {/* Card with input field */}
-      {/* <div className="flex-none shadow-md rounded px-60 py-10 bg-gray-50 m-auto">
-        <p className="font-bold text-2xl text-center">Set a meeting</p>
-        <form action="" className="form mt-4">
-          <div className="grid gap-x-32 grid-cols-2">
-            <div>
-              <div className="mb-3">
-                <label className="text-lg font-semibold" htmlFor="">
-                  Team Name:
-                </label>{" "}
-                <br />
-                <select className="rounded w-full border" name="" id="">
-                  <option value="">One</option>
-                  <option value="">Two</option>
-                  <option value="">Three</option>
-                </select>
-              </div>
-
-              <div>
-                <label className="text-lg font-semibold" htmlFor="">
-                  Meeting Agenda:
-                </label>{" "}
-                <br />
-                <textarea
-                  className="rounded w-full border"
-                  name=""
-                  placeholder="Agenda"
-                  cols={5}
-                  rows={3}
-                ></textarea>
-              </div>
-            </div>
-
-            <div>
-              <div className="mb-3">
-                <label className="text-lg font-semibold" htmlFor="">
-                  Meeting Title:
-                </label>{" "}
-                <br />
-                <input
-                  className="rounded border w-full"
-                  type="text"
-                  name=""
-                  placeholder="Title"
-                />
-              </div>
-
-              <div className="mb-3">
-                <label className="text-lg font-semibold" htmlFor="">
-                  Date:
-                </label>{" "}
-                <br />
-                <input className="rounded border w-full" type="date" name="" />
-              </div>
-
-              <div>
-                <label className="text-lg font-semibold" htmlFor="">
-                  time:
-                </label>{" "}
-                <br />
-                <input
-                  className="rounded border w-full"
-                  type="time"
-                  name=""
-                  id=""
-                />
-              </div>
-            </div>
-          </div>
-
-          <input
-            type="submit"
-            className="app-button"
-            value="Set meeting"
-          />
-        </form>
-      </div> */}
           <div className="mt-20 -ml-60 mr-3 lg:mr-0 lg:-ml-0 mb-4 grid px-3 lg:px-0 lg:mx-auto rounded-lg shadow w-full md:w-9/12 lg:w-1/2">
             <div className="pt-3">
               <h1 className="text-center text-gray-600 font-bold md:text-2xl text-xl">Set a meeting</h1>
