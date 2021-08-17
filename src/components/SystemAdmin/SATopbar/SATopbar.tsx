ab0339fbf3aa75d9bb2efa42ca85cddec5fc743c
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-08-18 02:45:26 +0600

    some file changed

--- src/components/SystemAdmin/SATopbar/SATopbar.tsx
+++ src/components/SystemAdmin/SATopbar/SATopbar.tsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import { SystemAdminDataContext } from '../../../Contexts/UserDataContext';
 
 const SATopbar = () => {
-    const { systemAdminData, setSystemAdminData } = useContext(SystemAdminDataContext);
+    const { systemAdminData } = useContext(SystemAdminDataContext);
     const [showSidebar, setShowSidebar] = useState(false);
+    const [showDropdown, setShowDropdown] = useState(false)
     const sidebar = document.getElementById('sa-sidebar');
     showSidebar ? sidebar?.classList.add('show') : sidebar?.classList.remove('show')
     return (
@@ -14,7 +15,23 @@ const SATopbar = () => {
                     <Link to="#" className="font-medium">Home</Link> /
                     <Link to="#" className="font-medium">Dashboard</Link>
                 </div>
-                <p className="username">{systemAdminData.name}</p>
+                <button
+                    onClick={() => setShowDropdown(!showDropdown)}
+                    className="username relative">
+                    {
+                        systemAdminData.name && systemAdminData.name
+                    }
+                    {
+                        !systemAdminData.name &&
+                        <span>Profile</span>
+                    }
+                    <div id="profile-dropdown" className="hidden absolute right-5 top-10 mt-2 py-2 w-48 bg-white rounded-md border shadow-xl z-20">
+                        <Link to="/view-sa-profile" className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-400 hover:text-white">
+                            View Profile
+                        </Link>
+                        <Link to="/system-admin/login" className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-400 hover:text-white" onClick={logOut}>Logout</Link>
+                    </div>
+                </button>
             </nav>
             <nav className="topbar without-sidebar px-4 text-2xl font-bold">
                 <h2 className="">E-Team</h2>
