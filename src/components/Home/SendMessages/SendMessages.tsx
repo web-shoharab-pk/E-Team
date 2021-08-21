94766cd0f8e0468fbc89105b01d5ee9594629bd3
MasumBilla19 <masumiraj0218@gmail.com>
2021-08-22 05:41:05 +0600

    someupdate

--- src/components/Home/SendMessages/SendMessages.tsx
+++ src/components/Home/SendMessages/SendMessages.tsx
@@ -4,28 +4,30 @@ import React, {useContext, useState} from 'react';
 import firebase from "firebase/app";
 import "firebase/firestore";
 import db from '../../Firebase/Firebase';
+import { ConpanyDataContext } from '../../../Contexts/UserDataContext';
 
 
 const SendMessages = () => {
     const [msg, setMsg] = useState('');
+    const {companyData, setCompanyData} = useContext<any>(ConpanyDataContext);
 
     async function sendMessage(e:any) {
         e.preventDefault();
-        // const {} 
+        const {co_id} = companyData;
+
         await db.collection('messages').add({
             text: msg,
+            co_id,
             createAt: firebase.firestore.FieldValue.serverTimestamp()
         })
-        .then(() => {
-            (document.getElementById("text") as HTMLInputElement).value = "";
-        })
+        setMsg('')
     }
 
     return (
         <div className="w-full mt-2 text-center">
             <form action="#" onSubmit={sendMessage}>
                 <div className="max-w-sm mx-auto p-1 pr-0 flex items-center">
-                    <input type="text" value={msg} onChange={(e) => setMsg(e.target.value)} id="text" placeholder="Write.." className="flex-1 appearance-none rounded-full shadow p-3 text-grey-dark mr-2 focus:outline-none" />
+                    <input type="text" value={msg} onChange={(e) => setMsg(e.target.value)} placeholder="Write.." required className="flex-1 appearance-none rounded-full shadow p-3 text-grey-dark mr-2 focus:outline-none" />
                     <button type="submit" className="appearance-none bg-blue-400 text-white text-base font-semibold tracking-wide px-4 py-3 rounded-full shadow hover:bg-indigo-light">
                         <FontAwesomeIcon icon={faPaperPlane} />
                     </button>
--- src/components/Home/SingleChatBoard/SingleChatBoard.tsx
+++ src/components/Home/SingleChatBoard/SingleChatBoard.tsx
@@ -1,21 +1,25 @@
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import SendMessages from '../SendMessages/SendMessages';
 import firebase from "firebase/app";
 import "firebase/firestore";
 import db from '../../Firebase/Firebase';
+import { ConpanyDataContext } from '../../../Contexts/UserDataContext';
 
 const SingleChatBoard = () => {
     const [messages, setMessages] = useState<any>([]);
+    const {companyData, setCompanyData} = useContext<any>(ConpanyDataContext);
 
     useEffect(() => {
-        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
-            // setMessages(snapshot.docs.map(doc=> doc.data()));
-            console.log(snapshot.docs)
+        // db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+        db.collection('messages').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+            console.log(snapshot)
         })
     }, [])
+
     return (
         <div className="relative">
             <div className="shadow fixed bg-white right-9 bottom-0 w-5/12 rounded-lg px-5 py-3">
@@ -35,54 +39,23 @@ const SingleChatBoard = () => {
                     </div>
                 </div>
             
-                <div className="mt-3">
-                    <div className="flex justify-start mb-2 h-auto">
-                        <div className="mr-5 mt-7 rounded-full h-8 px-1.5 border border-black text-lg">
-                            {/* <img src="" alt="" /> */}
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
+                <div className="mt-3 h-60 overflow-y-scroll">
+                    {
+                        messages.map(({id, text, co_id}:any) => (
+                            <div key={id} className={`msg ${co_id === companyData.co_id ? 'sent' : 'received'}`}>
+                                <div className="flex justify-start mb-2">
+                                    <div className="mr-5 mt-7 rounded-full h-8 px-1.5 border border-black text-lg">
+                                        {/* <img src="" alt="" /> */}
+                                        <FontAwesomeIcon icon={faUser} />
+                                    </div>
 
-                        <div className="bg-gray-300 py-2 px-4 rounded-sm">
-                            <p>Hello</p>
-                            <p>How Are you?</p>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-end mb-2 h-auto">
-                        <div className="bg-gray-300 py-2 px-4 rounded-sm">
-                            <p>Hello</p>
-                            <p>How Are you?</p>
-                        </div>
-
-                        <div className="ml-5 mt-7 rounded-full h-8 px-1.5 border border-black text-lg">
-                            {/* <img src="" alt="" /> */}
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
-                    </div>
-
-                    <div className="flex justify-start mb-2 h-auto">
-                        <div className="mr-5 mt-7 rounded-full h-8 px-1.5 border border-black text-lg">
-                            {/* <img src="" alt="" /> */}
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
-
-                        <div className="bg-gray-300 py-2 px-4 rounded-sm">
-                            <p>Hello</p>
-                            <p>How Are you?</p>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-end mb-2 h-auto">
-                        <div className="bg-gray-300 py-2 px-4 rounded-sm">
-                            <p>Hello</p>
-                            <p>How Are you?</p>
-                        </div>
-
-                        <div className="ml-5 mt-7 rounded-full h-8 px-1.5 border border-black text-lg">
-                            {/* <img src="" alt="" /> */}
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
-                    </div>
+                                    <div className="bg-gray-300 py-2 px-4 rounded-sm">
+                                        <p>{text}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <SendMessages />
